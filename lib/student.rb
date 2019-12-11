c09741d558bf275401f1d656918ede712d9bd11f
Nethelbert <gilblasse@gmail.com>
2019-12-11 18:27:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,5 @@
 class Student
   attr_accessor :id, :name, :grade
-  
-  def initialize(name,grade)
-    @id ||= nil
-    @name = name
-    @grade = grade
-  end
 
   def self.new_from_db(row)
     binding.pry
